927e38337b39d6fc7ffb47d8547cecb3e83406f2
Pavel <0x706a@gmail.com>
2024-02-15 22:47:29 +0200

    Implemented Hangman.inputChar method

--- src/test/HangmanTest.java
+++ src/test/HangmanTest.java
@@ -1,6 +1,20 @@
 package test;
 
+import main.Hangman;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HangmanTest {
+    @Test
+    public void HangmanInputCharTest() {
+        assert Hangman.inputChar("a") == 'a';
+        assert Hangman.inputChar("z") == 'z';
+        // check if these raise exceptions
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Hangman.inputChar("abc"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Hangman.inputChar("Z"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Hangman.inputChar(" "));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Hangman.inputChar(""));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Hangman.inputChar("aa aaa aa"));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Hangman.inputChar("1"));
+    }
 }
